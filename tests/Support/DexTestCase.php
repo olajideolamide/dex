50798d936fcbae55c3a84f4ca7c9b182b210caaa
Olajide <jidemac4@gmail.com>
2026-05-22 17:34:31 +0100

    updated tests

--- tests/Support/DexTestCase.php
+++ tests/Support/DexTestCase.php
@@ -47,14 +47,11 @@ abstract class DexTestCase extends CIUnitTestCase
 
     private function clearDexEnv(): void
     {
-        $superglobals = Services::superglobals();
-
         foreach (array_unique($this->envKeys) as $key) {
             putenv($key);
             unset($_ENV[$key]);
-            $server = $superglobals->getServerArray();
-            unset($server[$key]);
-            $superglobals->setServerArray($server);
+            /** @phpstan-ignore-next-line */
+            unset($_SERVER[$key]);
         }
 
         $this->envKeys = [];
