acc9bdfbbb187c66a3c2f75178a84333d00c2ae5
Olajide <jidemac4@gmail.com>
2026-05-22 16:33:32 +0100

    fix issues

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -6,7 +6,6 @@ DEPENDENCIES="${1:-highest}"
 case "${DEPENDENCIES}" in
   lowest)
     # Test against the minimum supported CI4 version, not just a generally low dependency set.
-    rm -f composer.lock
     composer update codeigniter4/framework:4.3.5 --with-all-dependencies --prefer-lowest --prefer-stable --prefer-dist --no-interaction --no-progress
     ;;
   highest)
