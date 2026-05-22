9f7683276a28052eafead12a6c5a0671ba5913bd
Olajide <jidemac4@gmail.com>
2026-05-22 16:41:01 +0100

    better tests

--- .github/scripts/install-dependencies.sh
+++ .github/scripts/install-dependencies.sh
@@ -6,7 +6,7 @@ DEPENDENCIES="${1:-highest}"
 case "${DEPENDENCIES}" in
   lowest)
     # Test against the minimum supported CI4 version, not just a generally low dependency set.
-    composer update codeigniter4/framework:4.3.5 --with-all-dependencies --prefer-lowest --prefer-stable --prefer-dist --no-interaction --no-progress
+    composer update --with codeigniter4/framework:4.3.5 --with-all-dependencies --prefer-lowest --prefer-stable --prefer-dist --no-interaction --no-progress
     ;;
   highest)
     composer update --prefer-dist --no-interaction --no-progress
